b184856e95501ea02233f16a4a5bc4ed1d5d741a
Michael-cuhk <michael.yunfei@gmail.com>
2019-06-28 14:48:11 +0200

    up date classficaiton - add k-cluster means

--- Classification/classification.py
+++ Classification/classification.py
@@ -9,6 +9,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.optimize import minimize
 import os
+import random
+import sklearn.linear_model as skl_lm
+from scipy.io import loadmat
 
 # check working directory
 os.getcwd()
@@ -96,7 +99,109 @@ LRgradient(initial_theta, X, Y)
 
 res = minimize(LRcostFunction, initial_theta, args=(X, Y.flatten()),
                method='BFGS', jac=LRgradient, options={'maxiter': 400})
-res.x
+res.x  # array([-25.16133284,   0.2062317 ,   0.2014716 ])
 res.fun
+
+
+# Gradient Ascent (bath version)
+# be careful, now we are maxmimizing the likelihood function
+# through the likelihood function, we have to update the coefficients
+# after calculating the gradient.
+# Warning: this one nees a very large number of interation
+
+def LGD(x, y, alpha, max):
+    '''
+    Though the name is called LGD(logistic gradient descent), with the log
+    likelihood function, it's more proper to call it logistic gradient descent
+    Input:
+    x - n by m matrix
+    y - n by 1 matrix
+    max - maxiterate
+    Theta (coefficents) will be generated inside the function
+
+    Output:
+    estimated coefficients
+    '''
+    n, m = x.shape  # get the shape of x: n by m
+    theta = np.array([0.0 for i in range(m)]).reshape(-1, 1)  # m by 1 vector
+    for i in range(max):
+        fx = sigmoid(x @ theta)
+        theta = theta - 1/n * alpha * x.T @ (fx-y)
+
+    return(theta)
+
+
+coef1 = LGD(X, Y, 0.001, 3000000)
+coef1
+# array([[-20.20395536],
+#        [  0.16661406],
+#        [  0.1613447 ]])
+
+
+def LSGD(x, y, alpha, max):
+    n, m = x.shape  # get the shape of x: n by m
+    theta = np.array([0.0 for i in range(m)]).reshape(-1, 1)  # m by 1 vector
+    for i in range(max):
+        randomIndex = random.sample(range(n), n)
+        xtrain = x[randomIndex]
+        ytrain = y[randomIndex]
+        for j in range(n):
+            x_j = xtrain[j].reshape(1, -1)
+            y_j = ytrain[j].reshape(-1, 1)
+            fx = sigmoid(x_j @ theta)
+            theta = theta - 1/n * alpha * x_j.T @ (fx-y_j)
+
+    return(theta)
+
+
+ceof2 =LSGD(X, Y, 0.001, 100000)
+ceof2
+# array([[-4.83184987],
+#        [ 0.045346  ],
+#        [ 0.03813775]])
+
+
+# gradient descent methods are not efficient for estimating coefficients of
+# logistic regressions. Newton's methods are better
+
+
+# K-means classification
+# Case studies from NG, Adrew
+
+kdata1 = loadmat('ex7data2.mat')
+kdata1.keys()
+kx1 = kdata1['X']
+plt.scatter(kx1[:,0], kx1[:,1], s=40, cmap=plt.cm.prism)
+
+# the plot implies there might be 3 clusters, our taks is to classify them
+# the key of doing k-cluster classificiation is to find the centroids
+
+initial_centroids = np.array([[3, 3], [6, 2], [8, 5]])
+initial_centroids.shape
+
+def Kcentroids(X, int_cent, stop_rule):
+    '''
+    The function to find the centroids for k-clusters
+    Input:
+    X - n by m matrix
+    ini_cent: k by m matrix
+    stop_rule: the rule for stoping the iteration when convegencing
+
+    output:
+    X with classification: b by (m+1) matrix
+    kcentroids: k by m matrix for centroids
+    '''
+    n, m = X.shape
+    k = int_cent.shape[0]
+
+
+
+
+
+
+
+
+
+
 
 # End of Code
